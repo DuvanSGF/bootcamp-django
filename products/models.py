31c17f09acac201966ab537c97c9795575438ecc
Duvan sgf <damejia.98@hotmail.com>
2019-07-01 22:23:24 -0500

    Registration of the model to admin

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    id          = models.AutoField(primary_key=True)
+    id_products = models.AutoField(primary_key=True)
     title       = models.CharField(max_length=25)
     description = models.TextField()
     price       = models.CharField(max_length=50)

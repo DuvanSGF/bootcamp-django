57bf8dbb4028e73e4ababa3155ba8898062314bf
Duvan sgf <damejia.98@hotmail.com>
2019-08-07 16:41:05 -0500

    Templates

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,10 @@ def home_view(request,*args, **kwargs):
     print(request.user)
     #return HttpResponse("<h1>Hello Ing. Duvan Mejia </h1>") #string of HTML code
     return render(request, "home.html", {})
+
+def about_view(request,*args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 8080
+    }
+    return render(request, "about.html", my_context)
--- web/urls.py
+++ web/urls.py
@@ -27,8 +27,9 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, about_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('about/', about_view, name='about'),
 ]

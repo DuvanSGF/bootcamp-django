3c13cdb770daebbff2db933160ded45e4d1b5f8b
Duvan sgf <damejia.98@hotmail.com>
2019-08-07 16:16:01 -0500

    templates

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello Ing. Duvan Mejia </h1>") #string of HTML code
+def home_view(request,*args, **kwargs):
+    print(request.user)
+    #return HttpResponse("<h1>Hello Ing. Duvan Mejia </h1>") #string of HTML code
+    return render(request, "home.html", {})
